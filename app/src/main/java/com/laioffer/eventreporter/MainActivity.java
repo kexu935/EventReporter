d61ffab24e97c59bac6abadf247073af610dfeb4
kexu935 <xukekekekeke@gmail.com>
2018-03-11 15:50:42 -0700

    communicate two fragments

--- app/src/main/java/com/laioffer/eventreporter/MainActivity.java
+++ app/src/main/java/com/laioffer/eventreporter/MainActivity.java
@@ -9,17 +9,31 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener
+{
+    private EventFragment mListFragment;
+    private CommentFragment mGridFragment;
+
+    @Override
+    public void onItemSelected(int position){
+        mGridFragment.onItemSelected(position);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 // Show different fragments based on screen size.
-        if (findViewById(R.id.fragment_container) != null) {
-            Fragment fragment = isTablet() ? new  CommentFragment() : new EventFragment();
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
+        //add list view
+        mListFragment = new EventFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.event_container,     mListFragment).commit();
+
+        //add Gridview
+        if (isTablet()) {
+            mGridFragment = new CommentFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.comment_container, mGridFragment).commit();
         }
+
     }
 
     private boolean isTablet() {
