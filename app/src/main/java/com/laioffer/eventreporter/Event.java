5d7925d3cbdf7b40487e8cedab7753dc2dfd5543
kexu935 <xukekekekeke@gmail.com>
2018-03-20 01:59:08 -0700

    redesign architecture

--- app/src/main/java/com/laioffer/eventreporter/Event.java
+++ app/src/main/java/com/laioffer/eventreporter/Event.java
@@ -4,7 +4,6 @@ package com.laioffer.eventreporter;
  * Created by xukek on 3/4/2018.
  */
 
-
 public class Event {
     /**
      * All data for a event.
@@ -12,20 +11,75 @@ public class Event {
     private String title;
     private String address;
     private String description;
+    private int like;
+    private String id;
+    private long time;
+    private String username;
+    private String imgUri;
 
-    /**
-     * Constructor
-     */
-    public Event(String title, String address, String description) {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    /**
-     * Getters for private attributes of Event class.
-     */
-    public String getTitle() { return this.title; }
-    public String getAddress() { return this.address; }
-    public String getDescription() { return this.description; }
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public Event() {}
 }
