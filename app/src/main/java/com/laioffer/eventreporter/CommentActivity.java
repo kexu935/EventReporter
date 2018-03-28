ae11b6b346ed6bec55a1f2a0490108ae52834f0d
kexu935 <xukekekekeke@gmail.com>
2018-03-27 22:43:10 -0700

    add comment feature

---
+++ app/src/main/java/com/laioffer/eventreporter/CommentActivity.java
@@ -0,0 +1,15 @@
+package com.laioffer.eventreporter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+
+    }
+
+}
--- app/src/main/java/com/laioffer/eventreporter/EventListAdapter.java
+++ app/src/main/java/com/laioffer/eventreporter/EventListAdapter.java
@@ -1,5 +1,7 @@
 package com.laioffer.eventreporter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
@@ -20,15 +22,17 @@ import java.util.List;
 public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder> {
     private List<Event> eventList;
     private DatabaseReference databaseReference;
+    private Context context;
 
     /**
      * Constructor for EventListAdapter
      *
      * @param events events that are showing on screen
      */
-    public EventListAdapter(List<Event> events) {
+    public EventListAdapter(List<Event> events, final Context context) {
         eventList = events;
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        this.context = context;
     }
 
     /**
@@ -120,6 +124,15 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
 
                     }
                 });
+            }
+        });
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CommentActivity.class);
+                String eventId = event.getId();
+                intent.putExtra("EventID", eventId);
+                context.startActivity(intent);
             }
         });
     }
